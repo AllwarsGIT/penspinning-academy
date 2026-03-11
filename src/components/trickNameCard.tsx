2994130c7c867038358ebb5cdc1c6a6ca8fb0019
DESKTOP-8CPSTED\Álvaro Sánchez Torre <allwarsps@gmail.com>
2026-03-11 18:45:13 +0100

    minor adjustments to styles

--- src/components/trickNameCard.tsx
+++ src/components/trickNameCard.tsx
@@ -21,7 +21,7 @@ function TrickNameCard({ title="", thumbnail="", href="", badge="" }: TrickNameC
     const color = badge ? difficultyColor[badge] : undefined;
 
     return (
-        <Link href={href} className="w-full rounded-xl overflow-hidden cursor-pointer group hover:scale-105 transition-all duration-500 ease-in-out">
+        <Link href={href} className="w-full rounded-xl overflow-hidden cursor-pointer group hover:scale-105 transition-all duration-300 ease-in-out">
             <div className="w-full aspect-video">
                 {thumbnail ? (
                     <Image 
@@ -33,7 +33,7 @@ function TrickNameCard({ title="", thumbnail="", href="", badge="" }: TrickNameC
                     <div className="w-full h-full bg-gray-200 dark:bg-gray-800" />
                 )}
                 <div className="p-3 flex flex-col gap-2 bg-whitePrimary dark:bg-blackPrimary">
-                    <h2 className="font-semibold group-hover:opacity-70">
+                    <h2 className="font-semibold ">
                         {title}
                     </h2>
                     {badge && (
