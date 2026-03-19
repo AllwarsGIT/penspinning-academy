dff0046d45da4e741dc948411a940d64ff532f76
DESKTOP-8CPSTED\Álvaro Sánchez Torre <allwarsps@gmail.com>
2026-03-19 21:52:34 +0100

    font

--- src/app/tricks/[slug]/components/modifierToggle.tsx
+++ src/app/tricks/[slug]/components/modifierToggle.tsx
@@ -13,12 +13,12 @@ function ModifierToggle({ modifierId, isActive, onToggle, options }: ModifierTog
             onClick={() => onToggle(modifierId)}
             className="flex flex-row gap-1 bg-white dark:bg-black p-1 my-2 rounded-xl w-fit cursor-pointer transition-colors duration-500 ease-in-out"
         >
-            <div className={`px-4 py-2 rounded-lg text-sm transition-all duration-200 ease-in-out ${
+            <div className={`px-4 py-2 rounded-lg  transition-all duration-200 ease-in-out ${
                 !isActive ? "bg-gray-200 text-black font-bold" : "text-gray-400"
             }`}>
                 {options[0]}
             </div>
-            <div className={`px-4 py-2 rounded-lg text-sm transition-all duration-200 ease-in-out ${
+            <div className={`px-4 py-2 rounded-lg  transition-all duration-200 ease-in-out ${
                 isActive ? "bg-gray-200 text-black font-bold" : "text-gray-400"
             }`}>
                 {options[1]}
