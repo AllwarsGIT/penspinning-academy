4cd79a8b72ed1583e56c3d6874d8c23d8a266d02
DESKTOP-8CPSTED\Álvaro Sánchez Torre <allwarsps@gmail.com>
2026-03-23 23:51:07 +0100

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,6 @@ import Header from "@/components/header";
 import Footer from "@/components/footer";
 import ScrollUpButton from "@/components/scrollUpButton";
 import SmoothScroll from "@/components/SmoothScroll"
-import PageTransition from "@/components/PageTransition"
 
 
 const geistSans = Geist({
@@ -27,9 +26,22 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "Penspinning Academy",
-  description: "Aiming to be the most powerful resource to learn penspinning.",
-};
+    title: "Penspinning Academy",
+    description: "Aiming to be the most powerful resource to learn penspinning.",
+    openGraph: {
+        title: "Penspinning Academy",
+        description: "Aiming to be the most powerful resource to learn penspinning.",
+        url: "https://penspinning-academy.vercel.app",
+        siteName: "Penspinning Academy",
+        images: [{ url: "/logo.svg", width: 1200, height: 630 }],
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "Penspinning Academy",
+        description: "Aiming to be the most powerful resource to learn penspinning.",
+        images: ["/logo.svg"],
+    }
+}
 
 export default function RootLayout({
   children,
