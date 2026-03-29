956d23bd23be5881dc6f92aa3380317e8a5e6aa0
DESKTOP-8CPSTED\Álvaro Sánchez Torre <allwarsps@gmail.com>
2026-03-29 17:38:21 +0200

    hotfix

--- src/components/discordInvite.tsx
+++ src/components/discordInvite.tsx
@@ -12,7 +12,7 @@ export default function DiscordButton({ inviteCode, label = "Join our Discord" }
             href={`https://discord.gg/${inviteCode}`}
             target="_blank"
             rel="noopener noreferrer"
-            className="flex items-center gap-2 px-4 py-2 bg-[#5865F2] hover:bg-[#4752c4] text-white rounded-lg transition-colors font-bold w-100"
+            className="flex items-center gap-2 px-4 py-2 bg-[#5865F2] hover:bg-[#4752c4] text-white rounded-lg transition-colors font-bold max-w-100"
         >
             <FaDiscord size={25} />
             {label}
